209ae408486aba9b8363d95094d8d8fafcbaa282
Art3mis15 <awesomeness.siew@gmail.com>
2025-01-13 16:03:49 -0800

    first commit

---
+++ CP-Making-a-Scene-Template-master/CP-Making-a-Scene-Template-master/src/main.js
@@ -0,0 +1,13 @@
+// Code Practice: Making a Scene
+// Name: Samantha Siew
+// Date: 1/13/25
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    scene: [ Play ],
+
+}
+
+let game = new Phaser.Game(config)
